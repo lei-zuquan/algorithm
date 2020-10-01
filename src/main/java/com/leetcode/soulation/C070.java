03c81bb39a368a0be13af97a6a40c4f926c55302
leizuquan1990 <843291011@qq.com>
2020-10-01 16:31:41 +0800

    # 新增：优化递归方式（通过记忆已经爬过的楼梯来降低时间复杂度）

--- src/main/java/com/leetcode/soulation/C070.java
+++ src/main/java/com/leetcode/soulation/C070.java
@@ -15,22 +15,47 @@ public class C070 {
         System.out.println(c070.climbStairs(5));
         System.out.println(c070.climbStairs(6));
         System.out.println(c070.climbStairs(44));
+
+        System.out.println("==================================");
+        System.out.println(c070.climbStairs2(1));
+        System.out.println(c070.climbStairs2(2));
+        System.out.println(c070.climbStairs2(3));
+        System.out.println(c070.climbStairs2(4));
+        System.out.println(c070.climbStairs2(5));
+        System.out.println(c070.climbStairs2(6));
+        System.out.println(c070.climbStairs2(44));
+
+        System.out.println("==================================");
+
+        System.out.println(c070.climbStairs1(1));
+        System.out.println(c070.climbStairs1(2));
+        System.out.println(c070.climbStairs1(3));
+        System.out.println(c070.climbStairs1(4));
+        System.out.println(c070.climbStairs1(5));
+        System.out.println(c070.climbStairs1(6));
+        System.out.println(c070.climbStairs1(44));
     }
 
-    public int climbStairs(int n) {
-        if (n == 6) {
-            return 13;
-        }
-        if (n == 5) {
-            return 8;
+    // 时间复杂度为O(1)
+    public int climbStairs1(int n) {
+        if (n <= 0) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int prevStep1 = 2;
+        int prevStep2 = 1;
+        for (int i = 3; i <= n; i++) {
+            int temp = prevStep1 + prevStep2;
+            prevStep2 = prevStep1;
+            prevStep1 = temp;
         }
 
-        if (n == 4) {
-            return 5;
-        }
-        if (n == 3) {
-            return 3;
-        }
+        return prevStep1;
+    }
+
+    // 递归方式，适用于n比较小的情况，如果N较大，则性能会急剧下降
+    // 递归方式：时间复杂度O(n^2)、空间复杂度为O(n)
+    public int climbStairs(int n) {
         if (n == 2) {
             return 2;
         }
@@ -40,4 +65,30 @@ public class C070 {
 
         return climbStairs(n-1) + climbStairs(n-2);
     }
+
+    // 改进的递归方法
+    // 我们将之前宽过楼梯的进行记忆
+    public int climbStairs2(int n) {
+        int[] arr = new int[n+1];
+        return climbStairs22(n, arr);
+    }
+
+    public int climbStairs22(int n, int[] arr) {
+        if (arr[n] != 0) return arr[n];
+
+        if (n == 2) {
+            arr[2] = 2;
+            return 2;
+        }
+        if (n == 1) {
+            arr[1] = 1;
+            return 1;
+        }
+
+        int ways = climbStairs22(n - 1, arr) + climbStairs22(n - 2, arr);
+        arr[n] = ways;
+        return ways;
+    }
+
+
 }
