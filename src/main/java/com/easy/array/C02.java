3ea0a1b7ee98c58c1499e33ee444f8c49c20aac3
leizuquan1990 <843291011@qq.com>
2020-10-07 21:46:10 +0800

    # 新增：122. 买卖股票的最佳时机 II

---
+++ src/main/java/com/easy/array/C02.java
@@ -0,0 +1,96 @@
+package com.easy.array;
+
+/**
+ * @Author:
+ * @Date: 2020-10-07 20:35
+ * @Version: 1.0
+ * @Modified By:
+ * @Description:
+ */
+
+/*
+122. 买卖股票的最佳时机 II
+
+给定一个数组，它的第i 个元素是一支给定股票第 i 天的价格。
+设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+
+示例 1:
+输入: [7,1,5,3,6,4]
+输出: 7
+解释: 在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出,
+这笔交易所能获得利润 = 5-1 = 4 。
+    随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出,
+    这笔交易所能获得利润 = 6-3 = 3 。
+
+示例 2:
+输入: [1,2,3,4,5]
+输出: 4
+解释: 在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出,
+    这笔交易所能获得利润 = 5-1 = 4 。
+    注意你不能在第 1 天和第 2 天接连购买股票，之后再将它们卖出。
+    因为这样属于同时参与了多笔交易，你必须在再次购买前出售掉之前的股票。
+
+示例3:
+输入: [7,6,4,3,1]
+输出: 0
+解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+
+提示：
+1 <= prices.length <= 3 * 10 ^ 4
+0 <= prices[i]<= 10 ^ 4
+
+
+ */
+public class C02 {
+    public static void main(String[] args) {
+        int[] arr1 = {7,1,5,3,6,4};
+        int[] arr2 = {1,2,3,4,5};
+        int[] arr3 = {7,6,4,3,1};
+        int[] arr4 = {1,9,6,9,1,7,1,1,5,9,9,9};
+
+        C02 c02 = new C02();
+        //System.out.println(c02.maxProfit(arr1));
+        //System.out.println(c02.maxProfit(arr2));
+        //System.out.println(c02.maxProfit(arr3));
+        System.out.println(c02.maxProfit(arr4));
+    }
+
+    public int maxProfit(int[] prices) {
+        // 如果数组元素只有一个或0个，则直接返回0
+        if (prices.length <= 1) return 0;
+
+        int lastMinIndex = 0;
+        int lastMaxIndex = -1;
+        int sum = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[lastMinIndex]) {
+                // 出现股价比最低股价要高的点
+                if (lastMaxIndex == -1) {
+                    lastMaxIndex = i;
+                } else if (prices[i] >= prices[lastMaxIndex]) {
+                    lastMaxIndex = i;
+                }
+            }
+
+            // 最高价出现拐点，即可卖出
+            boolean normalDownPoint = lastMaxIndex > lastMinIndex && prices[i] < prices[lastMaxIndex];
+            boolean endShouldSell = lastMaxIndex > lastMinIndex
+                    && prices[lastMaxIndex] > prices[lastMinIndex]
+                    && lastMaxIndex == prices.length -1;
+
+            if (normalDownPoint || endShouldSell) {
+                int currProfit = prices[lastMaxIndex] - prices[lastMinIndex];
+                sum += currProfit;
+                lastMinIndex = lastMaxIndex;
+                lastMaxIndex = -1;
+            }
+
+            // 向前搜索找到当前股价低谷
+            if (prices[i] < prices[lastMinIndex]) {
+                lastMinIndex = i;
+            }
+        }
+        return sum;
+    }
+}
