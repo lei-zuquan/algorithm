ea906f2b9f00edf8fbf3503c829f1ccd50727e85
leizuquan <843291011@qq.com>
2020-10-15 10:29:19 +0800

    # 新增：1002. 查找常用字符，新增计数法

--- src/main/java/com/leetcode/soulation/c1000/C1002.java
+++ src/main/java/com/leetcode/soulation/c1000/C1002.java
@@ -1,6 +1,7 @@
 package com.leetcode.soulation.c1000;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -50,8 +51,33 @@ public class C1002 {
         System.out.println("\n===============");
     }
 
-
+    // 计数法
     public List<String> commonChars(String[] A) {
+        int[] minFreq = new int[26];
+        Arrays.fill(minFreq, Integer.MAX_VALUE);
+        for (String word: A) {
+            int[] freq = new int[26];
+            int length = word.length();
+            for (int i = 0; i < length; ++i) {
+                char ch = word.charAt(i);
+                ++freq[ch - 'a'];
+            }
+            for (int i = 0; i < 26; ++i) {
+                minFreq[i] = Math.min(minFreq[i], freq[i]);
+            }
+        }
+
+        List<String> ans = new ArrayList<String>();
+        for (int i = 0; i < 26; ++i) {
+            for (int j = 0; j < minFreq[i]; ++j) {
+                ans.add(String.valueOf((char) (i + 'a')));
+            }
+        }
+        return ans;
+    }
+
+
+    public List<String> commonChars2(String[] A) {
         if (A == null || A.length == 0) return null;
 
         int length = A.length;
